0313c832258377cacd94d74713b5cbb345430603
WARIS ALI <abdulwaarisinfo@gmail.com>
2026-08-24 01:30:58 +0500

    Add keywords for £10k and £10,000 to index.py

--- index.py
+++ index.py
@@ -187,7 +187,7 @@ INDUSTRIES = {
         "keywords": [
             
               "cross-border", "cross border", "cross border payment", "cross-border payments", "bank froze my", "transaction blocked", "£15k", "£15,000",
-            "payment", "payments", "payment gateway", "payment processor", "payment processing", "transfer pending",
+            "payment", "payments", "payment gateway", "payment processor", "payment processing", "transfer pending", "£10k", "£10,000",
             "payment infrastructure", "payment platform", "payment provider", "payment rails",  
             "remittance", "remittances", "remit money", "forex", "fx trading", "fx rate", "transfer failed",
             "fx hedging", "wire transfer", "swift transfer", "ach transfer", "sepa transfer", "looking for a transfer",
