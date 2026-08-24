f3442d5d90a7d84b34729d85d1c0be01a57b857b
WARIS ALI <abdulwaarisinfo@gmail.com>
2026-08-24 17:00:50 +0500

    Add '750 thousand' to payment keywords list

--- index.py
+++ index.py
@@ -187,7 +187,7 @@ INDUSTRIES = {
         "keywords": [
             
               "cross-border", "cross border", "cross border payment", "cross-border payments", "bank froze my", "transaction blocked", "£15k", "£15,000",
-            "payment", "payments", "payment gateway", "payment processor", "payment processing", "transfer pending", "£10k", "£10,000",
+            "payment", "payments", "payment gateway", "payment processor", "payment processing", "transfer pending", "£10k", "£10,000", "750 thousand",
             "payment infrastructure", "payment platform", "payment provider", "payment rails",  
             "remittance", "remittances", "remit money", "forex", "fx trading", "fx rate", "transfer failed", "one million", "$1m",
             "fx hedging", "wire transfer", "swift transfer", "ach transfer", "sepa transfer", "looking for a transfer", "$1 million",
